e131b67dccf2748db67cc18c79fef9eb844f013b
Carmen Li <cmnli95@gmail.com>
2023-11-11 09:15:50 -0500

    pushing phase 1 javascript function continued carmen li

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity){
+    if (activity === undefined){
+        return 'This Saturday, I want to roller-skate!';
+    } else {
+        return 'This Saturday, I want to bathe my dog!';
+    }
+}
+
+function mondayWork(activity2){
+    if (activity2 === undefined){
+        return 'This Monday, I will go to the office.';
+    } else {
+        return 'This Monday, I will work from home.';
+    }
+}
+
+function wrapAdjective(flair){
+    return function(adjective="special"){
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
